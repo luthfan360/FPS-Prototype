a48aa97d5e6980e34a403ea8af27c90d464fed52
luthfan360 <luthfan360@gmail.com>
2022-01-14 21:01:30 +0700

    Added weapon and raycast

---
+++ Assets/Game/Scripts/LookX.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookX : MonoBehaviour
+{
+    [SerializeField]
+    float sensitivity = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.y += (mouseX * sensitivity);
+        transform.localEulerAngles = newRotation;
+    }
+}
--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
@@ -13,16 +14,48 @@ public class Player : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        CalculateMovement();
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0));
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                Debug.Log("You hit: " + hitInfo.transform.name);
+            }
+        }
+    }
+
+    void CalculateMovement()
     {
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(xInput, 0, yInput);
         Vector3 movement = direction * speed;
         movement.y -= gravity;
+
+        movement = transform.transform.TransformDirection(movement);
         controller.Move(movement * Time.deltaTime);
+
+        transform.position = GetComponent<NavMeshAgent>().nextPosition;
     }
 }
